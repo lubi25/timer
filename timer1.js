7dd458b1ea21b28b4e37e00e2c7402a9d7d6fd01
Lubi Islam <lubaba.islam91@gmail.com>
2024-02-28 22:59:40 -0500

    Updated code so that timer stops if there is no number provided

--- timer1.js
+++ timer1.js
@@ -1,7 +1,7 @@
 const validTime = process.argv.slice(2);
 
 const timerBeep = function(time) {
-  if (typeof time === 'number' && time > 0) {   // only goes off if 'time' is a number and > 0
+  if (typeof time === 'number' && time > 0 && time !== 'undefined') {   // only goes off if 'time' is a number and > 0
     setTimeout(() => {
       process.stdout.write('\x07');
     }, time * 1000);
